4a75af49001615e71dff891537a4dd79d63ce191
Kingsleyaham <kingsleyaham6@gmail.com>
2026-07-21 06:12:01 +0100

    fix: template bug

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from "tsdown";
+import { defineConfig } from 'tsdown';
 
 export default defineConfig({
-  entry: ["./src/server.ts", "./src/worker.ts"],
-  format: "esm",
-  outDir: "dist",
+  entry: ['./src/server.ts', './src/worker.ts'],
+  format: 'esm',
+  outDir: 'dist',
   clean: true,
   sourcemap: true,
-  tsconfig: "tsconfig.build.json",
+  tsconfig: 'tsconfig.build.json',
+  copy: [
+    {
+      from: 'templates',
+      to: 'dist',
+    },
+  ],
 });
